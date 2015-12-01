c2f1781507de889192685e0ae18c32f36f8f2df7
Elio Tanke <Elio@utmuser147-48.wireless.utoronto.ca>
2015-12-01 04:22:48 -0500

    added changes

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
 app.post('/sent', function(req, res) {
-    res.render('pages/index');
+	var name = req.body.name;
+	var message = req.body.message;
+    res.render('pages/sent', {
+    	name: name,
+    	message: message
+    });
 });
 
 var server = app.listen(3000, function () {
