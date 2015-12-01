2ebea84c1c82ef7cc6b9ca9b23bdec962fd02aa8
Elio Tanke <Elio@utmuser147-48.wireless.utoronto.ca>
2015-12-01 02:13:21 -0500

    adding ejs template and renaming app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendfile(path.join(__dirname + '/index.ejs'));
+});
+
+app.get('/sent', function(req, res) {
+    res.sendfile(path.join(__dirname + '/index.ejs'));
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
