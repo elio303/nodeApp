9f7d33fff6d0160c6045f7697574549500ae530c
Elio Tanke <Elio@utmuser147-48.wireless.utoronto.ca>
2015-12-01 05:08:45 -0500

    fixed port and server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
 var path = require('path');
 var bodyParser = require('body-parser');
 
+app.set('port', (process.env.PORT || 5000));
 app.set('views',  __dirname + '/views/pages');
 app.set('view engine', 'ejs');
 
@@ -22,9 +24,6 @@ app.post('/sent', function(req, res) {
     });
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+server.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
