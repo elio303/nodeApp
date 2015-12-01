3f077cbad4dad73aceac1d8b56222c86fd3e3d83
Elio Tanke <Elio@utmuser147-48.wireless.utoronto.ca>
2015-12-01 05:02:26 -0500

    fixed local path

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 
+app.set('views',  __dirname + '/views/pages');
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    res.render('index');
 });
 
 app.post('/sent', function(req, res) {
 	var name = req.body.name;
 	var message = req.body.message;
-    res.render('pages/sent', {
+    res.render('sent', {
     	name: name,
     	message: message
     });
